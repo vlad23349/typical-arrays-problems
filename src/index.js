a4b8c7975fde7dccc789361bfbd1c6269bbf8713
vlad23349 <78217932+vlad23349@users.noreply.github.com>
2021-03-09 20:15:39 +0300

    fix: add code for min max average

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
-exports.min = function min (array) {
-  return 0;
+
+exports.min = function min(array) {
+  if (array === undefined || (!array.length)) return 0;
+  let min = 9999999;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) min = array[i];
+  }
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined || (!array.length)) return 0;
+  let max = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > max) max = array[i];
+  }
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined || (!array.length)) return 0;
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return (sum / array.length);
 }
